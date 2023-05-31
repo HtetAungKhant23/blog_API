835f0fb12abb1969053eed333d35fb72b8bcfd2f
htetaungkhant <htetaung23.ha@gmail.com>
2023-05-31 20:13:30 +0630

    post model desgin

---
+++ models/post.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'post title is required!'],
+        tirm: true
+    },
+    description: {
+        type: String,
+        required: [true, 'post description is required!']
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: [true, 'post category is required!']
+    },
+    numViews: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    likes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    disLikes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please Author is required!']
+    },
+    photo: {
+        type: String,
+        required: [true, 'post image is required!']
+    }
+}, {timestamps: true});
